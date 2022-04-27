d87badb1575e1eac29535033311bb4777d32ec8a
Pedro Reckel Roberte <pedroreckel90@gmail.com>
2022-04-26 23:52:15 -0300

    Corrigindo formulário

--- htmlCss/validacaoFormulario/validacao.js
+++ htmlCss/validacaoFormulario/validacao.js
@@ -2,6 +2,9 @@ var modal = document.getElementById('id01');
 var nome = document.getElementById('nome')
 var senha = document.getElementById('senha')
 var botao = document.getElementById('botao')
+var idade = document.getElementById('idade')
+var setor = document.getElementById('setor')
+var data = document.getElementById('data')
 
 // Quando o usuário clicar em qualquer lugar fora do modal, ele fecha!
 window.onclick = function(event) {
@@ -13,9 +16,19 @@ window.onclick = function(event) {
 botao.addEventListener("click", function(e) {
     e.preventDefault()
     const valorNome = nome.value
-    const valorSenha = senha.valor
+    const valorSenha = senha.value
+    const valorIdade = idade.value
+    const valorSetor = setor.value
+    const valorData = data.value
+    const dataAtual = new Date()
 
-    if (valorNome === "" || valorSenha === "") {
+    if (valorNome === "" || valorSenha === "" || valorIdade === "" || valorSetor === "") {
         alert(`Preencher todos os campos!`)
+    } else if (valorIdade < 18) {
+        alert(`Você é menor de 18 anos!`)
+    } else if (valorNome.length < 5) {
+        alert(`O nome tem que ter pelo menos 5 cararacteres`)
+    } else if (valorData ) {
+
     }
 })
